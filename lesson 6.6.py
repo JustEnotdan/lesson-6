e22fb67cfecabc7437210a2189af3a829ff26bed
EnotDan <enotdan2020@gmai.com>
2021-06-04 10:12:43 +0300

    New hw

--- lesson 6.6.py
+++ lesson 6.6.py
@@ -3,8 +3,14 @@ a.write("5978,5\n8914,3\n7879,1\n1573,7")
 num = input("Введите число(если их несколько, записывайте через пробел):   ")
 a.seek(0)
 if num == "":
-    print(a.read())
+    b = a.readlines()
+    for i in b:
+        print(i.replace("\n", ""))
 elif len(num.split()) == 1:
-    print(a.readlines()[int(num)-1:])
+    b = a.readlines()[int(num)-1:]
+    for i in b:
+        print(i.replace("\n", ""))
 elif len(num.split()) == 2:
-    print(a.readlines()[int(num.split()[0]) - 1: int(num.split()[1])])
+    b = a.readlines()[int(num.split()[0]) - 1: int(num.split()[1])]
+    for i in b:
+        print(i.replace("\n", ""))
